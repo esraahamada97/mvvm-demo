34f0346e10dd3504d783023f601d2d0f94e0832d
Esraa <esraa.hamada@ibtikar.net.sa>
2020-09-06 10:02:50 +0200

    [app]: structure work done: 1- configrations schema 2- enviroment

---
+++ Mvvvm-demo/Resources/Configrations/Enviroment.swift
@@ -0,0 +1,45 @@
+//
+//  Enviroment.swift
+//  Mvvvm-demo
+//
+//  Created by Esraa Abuseada on 9/6/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Foundation
+
+// swiftlint:disable all
+enum Environment {
+    
+    // MARK: - Keys
+    enum Keys {
+        enum Plist {
+            static let baseURL = "BASE_URL"
+            static let apiKey = "API_KEY"
+        }
+    }
+    
+    // MARK: - Plist
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist file not found")
+        }
+        return dict
+    }()
+    
+    // MARK: - Plist values
+    static let baseURL: String = {
+        guard let rootURLstring = Environment.infoDictionary[Keys.Plist.baseURL] as? String else {
+            fatalError("Root URL not set in plist for this environment")
+        }
+        let baseUrl = "https://\(rootURLstring)"
+        return baseUrl
+    }()
+    
+    static let apiKey: String = {
+        guard let apiKey = Environment.infoDictionary[Keys.Plist.apiKey] as? String else {
+            fatalError("API Key not set in plist for this environment")
+        }
+        return apiKey
+    }()
+}
